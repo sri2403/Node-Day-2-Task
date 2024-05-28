6cb282d3fa6b8d5e4cd401a5a5199b93c679272c
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-05-28 10:59:27 +0530

    endpoints added

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use("/api",rRouter)
 app.get("/",(req,res)=>{
     res.status(200).send( `<div style="font-size:22px">
     <div style="text-align: center"><strong> Nodejs hall booking api task </strong></div>
-    <p>Firstly, I added some data to the room variable. Using the GET method, I retrieved all the room details from the API.Change the endpoint to <strong> /allRoomDetails </strong> to retrieve all room's data.</p>
-    <ol><li><p><strong> /createRoom </strong>: Create a new room<p></li>
-    <li><p><strong> /roomBooking </strong>: Book a room<p></li>
-    <li><p><strong> /bookedRoomData </strong>: Retrieve booked room's data<p></li>
-    <li><p><strong> /customerData </strong>: Retrieve all customer's data<p></li>
-    <li><p><strong> /customerCount </strong>: See the customer's room booking count</li></ol><p>
+    <p>Firstly, I added some data to the room variable. Using the GET method, I retrieved all the room details from the API.Change the endpoint to <strong> /api/allRoomDetails </strong> to retrieve all room's data.</p>
+    <ol><li><p><strong> /api/createRoom </strong>: Create a new room<p></li>
+    <li><p><strong> /api/roomBooking </strong>: Book a room<p></li>
+    <li><p><strong> /api/bookedRoomData </strong>: Retrieve booked room's data<p></li>
+    <li><p><strong> /api/customerData </strong>: Retrieve all customer's data<p></li>
+    <li><p><strong> /api/customerCount </strong>: See the customer's room booking count</li></ol><p>
 
     </div>`);
     
